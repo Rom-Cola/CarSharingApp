45ab842f1c043a7c45094d2fcbd845806cfc0b5d
Loiev Roman <loievroman@gmail.com>
2025-08-13 00:15:58 +0300

    Initial commit

---
+++ src/test/java/com/loievroman/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loievroman.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
